acd67fd71e40bfbfbaaff996abda3496ba298b88
Witherlord <ruslankotvytskyi@gmail.com>
2020-07-11 14:47:29 +0800

    Create Bubble Sort.py

---
+++ Sort/Bubble Sort.py
@@ -0,0 +1,30 @@
+'''
+Bubble sort algo. O(N*log(N)) T / O(1) S
+'''
+
+def bubbleSort(array):
+    
+	if array is None:
+		return None
+	
+	lenArray = len(array)
+	
+	if lenArray == 1 or lenArray == 0:
+		return array
+	
+	while True:
+		wasSwap = False
+		index = 0
+		while index < (lenArray - 1):
+			if array[index] > array[index + 1]:
+				array[index], array[index+1] = array[index+1], array[index]
+				wasSwap = True
+			index += 1
+			
+		if wasSwap == False:
+			return array
+		lenArray -= 1
+			
+			
+			
+			
