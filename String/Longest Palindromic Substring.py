2a8fcc5ef56899c49af9c825f027bc2f14ab52a5
Witherlord <ruslankotvytskyi@gmail.com>
2020-07-12 11:24:04 +0800

    Update Longest Palindromic Substring.py

--- String/Longest Palindromic Substring.py
+++ String/Longest Palindromic Substring.py
@@ -17,69 +17,34 @@ Input : "aaaabaaa"
 Output : "aaabaaa"
 '''
 
-
-class Solution:
-    # @param A : string
-    # @return a strings
-    def longestPalindrome(self, A):
-        
-        if len(A) < 1:
-            return ''
-        elif len(A) == 1:
-            return A
-            
-        l = 0
-        r = 0
-        maxi = 0
-        index_l = 0
-        index_r = 0
-
-        for i in range(len(A)):
-            
-            l, r = Solution.expandMid(A, i, i)
-            
-            if maxi < (r - l + 1):
-                maxi = r - l + 1
-                index_l = l
-                index_r = r
-            
-            l, r = Solution.expandMid(A, i, i+1)
-            
-            if maxi < (r - l + 1):
-                maxi = r - l + 1
-                index_l = l
-                index_r = r
-
-        return A[index_l:index_r+1]
-        
-        
-
-    def expandMid(s, left, right):
-        
-        st = 0
-        e = 0
-        flag = False
-        
-        while left >= 0 and right < len(s):
-            
-            if s[left] == s[right]:
-                st = left
-                e = right
-                flag = True
-            else:
-                break
-        
-            left -= 1
-            right += 1
-        
-        if flag == True:
-            return st, e
-        else:
-            return 0, 0
-            
-            
-            
-            
-            
-                    
-                    
+def longestPalindromicSubstring(string):
+	
+	if len(string) <= 0:
+		return None
+	
+    palindrom_index = [0, 0]
+	
+	for i in range(len(string)-1):
+		
+		left, right = palindromHelper(string, i, i)
+		if (right - left) > (palindrom_index[1] - palindrom_index[0]):
+			palindrom_index = [left, right]
+			
+		left, right = palindromHelper(string, i, i+1)
+		if (right - left) > (palindrom_index[1] - palindrom_index[0]):
+			palindrom_index = [left, right]
+		
+	return string[palindrom_index[0]:palindrom_index[1]+1]
+		
+def palindromHelper(string, left, right):
+	while left >= 0 and right < len(string):
+		if string[left] != string[right]:
+			break		
+		left -= 1
+		right += 1
+		
+	return [left+1, right-1]
+			
+		
+			
+	
