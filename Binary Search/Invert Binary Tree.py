9d843c2a9a4a3cd8aa9b2fee34407260c0520735
Ruslan <ruslankotvytskyi@gmail.com>
2020-07-18 16:23:38 +0800

    Update and rename Binary Search/Invert Binary Tree.py to Binary Search Tree/Invert Binary Tree.py

--- Binary Search/Invert Binary Tree.py
+++
@@ -1,9 +0,0 @@
-def invertBinaryTree(tree):
-    nodeInvert(tree)
-	if tree.left is not None:
-		invertBinaryTree(tree.left)
-	if tree.right is not None:
-		invertBinaryTree(tree.right)
-
-def nodeInvert(tree):
-	tree.left, tree.right = tree.right, tree.left
