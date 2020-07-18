eb74f1a32a350e740978771d82bf90b5ea21244e
Ruslan <ruslankotvytskyi@gmail.com>
2020-07-18 15:58:25 +0800

    Update Move Element To End.py

--- Array/Move Element To End.py
+++ Array/Move Element To End.py
@@ -1,9 +1,24 @@
 '''
-Move given elements from the array to the end: [2, 1, 2, 3] => [1, 3, 2, 2] / [3, 1, 2, 2]
+Move given elements from the array to the end: [2, 1, 2, 3] => Ver1 - [1, 3, 2, 2] / Ver2 - [3, 1, 2, 2]
+Ver1: Swapping with remembering of toMove starting point.
+Ver2: 2 pointers at the start/end. At first check end, then move left.
 '''
 
 def moveElementToEnd(array, toMove):
-  left = 0
+	start_point = -1
+	for i in range(len(array)):
+		if array[i] != toMove and start_point >= 0:
+			array[i], array[start_point] = array[start_point], array[i]
+			start_point += 1
+			if array[start_point] != toMove:
+				start_point = -1
+		else:
+			if array[i] == toMove and start_point == -1:
+				start_point = i
+	return array
+
+def moveElementToEnd(array, toMove):
+  	left = 0
 	right = len(array)-1
 	while (right-left) > 0:
 		if array[right] == toMove:
