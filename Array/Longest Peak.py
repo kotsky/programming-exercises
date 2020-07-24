0ff93629ef9d07d43d2ff13babd5b7a3d1e9956f
Ruslan <ruslankotvytskyi@gmail.com>
2020-07-24 16:05:50 +0800

    Create Longest Peak.py

---
+++ Array/Longest Peak.py
@@ -0,0 +1,27 @@
+'''
+To find the longest peak in the array. [1, 1, 2, 3, 2, 2] => [1, 2, 3, 2] = 4
+
+First, to find it's peak, then go in left and right to find its range.
+'''
+def longestPeak(array):
+    if len(array) < 3:
+        return 0
+
+    peak_lenght = 0
+	i = 1
+    while i < len(array)-1:
+		if array[i-1] < array[i] and array[i+1] < array[i]: # if there is a peak
+			j = i
+			while j >= 1 and array[j-1] < array[j]:
+				j -= 1
+			start_index = j
+			j = i
+			while array[j+1] < array[j]:
+				j += 1
+				if j == len(array)-1:
+					break
+			peak_lenght = max(peak_lenght, j - start_index + 1)
+			i = j
+		i += 1
+        
+    return peak_lenght
