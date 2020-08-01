59f2f8ac48f0aea24a770fe61f9dadca49d710dd
Ruslan <ruslankotvytskyi@gmail.com>
2020-08-01 14:30:28 +0800

    Update Shorten Path.py

--- Stacks And Queues/Shorten Path.py
+++ Stacks And Queues/Shorten Path.py
@@ -11,6 +11,33 @@ expected = "../../../../../../../.."
 # Version 1
 # Filling answer array from the beginning. Pop value, if we meet "..".
 # Add "" 1st elem if 1st is "/". To handle relative path with "../../........."
+def shortenPath(path):
+    isRoot = path[0] == '/'
+	tokens = filter(useful, path.split('/'))
+	stack = []
+	if isRoot:
+		stack.append('')
+	
+	for token in tokens:
+		if token != '..':
+			stack.append(token)
+		else:
+			if len(stack) == 0 or stack[-1] == '..':
+				stack.append(token)
+			elif stack[-1] != '':
+				stack.pop()
+	
+	if len(stack) == 1 and stack[0] == '':
+		return '/'
+	
+	return '/'.join(stack)
+			
+	
+
+def useful(string):
+	return len(string) > 0 and string != '.'
+
+
 
 # Version 2
 
