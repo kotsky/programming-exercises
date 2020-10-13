24804a91fcc8b8fab0a9c45ed284ca71e2bba113
Ruslan <ruslankotvytskyi@gmail.com>
2020-10-13 13:32:08 +0800

    Update Airport Connections.py

--- Graph/Airport Connections.py
+++ Graph/Airport Connections.py
@@ -7,6 +7,7 @@ Note that routes only allow you to fly in one direction => ["1", "2"] means 1 ->
 '''
 
 '''
+Version 1:
 The idea behind is 1) to define, which airports are unreacable from the start airport and 
 2) to create routes from the start airport to unvisited airports (we say: let's we have route 
 from the start airport to particular one -> add this particular airport to new_routes) and explore (DFS method),
@@ -22,9 +23,15 @@ we go out from the exploring when:
 3) calculate from new_routes, how many routes were needed (by their property "True").
 
 Easy.
+
+Version 2:
+Less optimal from the time complexity point of view.
+The idea is to assign "value" to each unreached (unvisited) airport, which shows how many airports can be reached 
+from that particular start airport. Then, sort these unreaced airports by their .value from max to min.
+And Count how many new routes you need to go visit all airports.
 '''
 
-
+# Version 1.
 # O(a + r) Time / O(a + r) Space, where a - airports, r - routes
 def airportConnections(airports, routes, startingAirport):
     airport_connections = AirportsConnections(airports, routes, startingAirport)  # O(a + r) Time / O(a + r) Space
@@ -161,3 +168,97 @@ routes = [
         ]
 
 print(airportConnections(AIRPORTS, routes, STARTING_AIRPORT))
+
+
+'''
+# Version 2.
+# O(a * (a + r) + (a + r) + a * log(a)) Time / O(a + r) Space
+def airportConnections(airports, routes, startingAirport):
+    airport_connections = SpecialGraphAirportConnections(airports, routes, startingAirport)
+    airport_connections.markUnreachableAirports(airport_connections.starting_airport)
+    list_of_unreachable_airports = assignValueToUnreachableAirports(airport_connections)
+    list_of_unreachable_airports.sort(key=lambda x: -x.value)
+    return countNewRoutes(list_of_unreachable_airports, airport_connections)
+
+
+def countNewRoutes(airport_nodes, airport_connections):
+    number_of_new_routes = 0
+    # output_list = []
+    for airport_node in airport_nodes:
+        if airport_node.reachable:
+            continue
+        number_of_new_routes += 1
+        # output_list.append(airport_node.airport_name)
+        airport_connections.markUnreachableAirports(airport_node)
+        airport_node.reachable = False
+    return number_of_new_routes     # , output_list
+
+
+def assignValueToUnreachableAirports(airport_connections):
+    airport_connections = airport_connections.airport_connections
+    list_of_unreachable_airports = []
+    for airport in airport_connections.keys():
+        airport_node = airport_connections[airport]
+        if airport_node.reachable:
+            continue
+        assignValueToAirport(airport_node, airport_node, True)
+        list_of_unreachable_airports.append(airport_node)
+    return list_of_unreachable_airports
+
+
+def assignValueToAirport(airport_node, start_airport_node, reference):
+    if not reference and airport_node == start_airport_node:  # mark initial start point as a reserence 
+        return -1                                             # to avoid wrong assigment in inner loops
+
+    if airport_node.reachable:
+        return 0
+
+    airport_node.reachable = True # mark it to avoid infinite loop
+
+    local_value = 0
+    for next_airport in airport_node.routesTo:
+        local_value += assignValueToAirport(next_airport, start_airport_node, False) + 1
+
+    if reference:
+        start_airport_node.value = local_value
+
+    airport_node.reachable = False  # unmark for further processing to build new routes
+
+    return local_value
+
+
+class AirportConnectionNode:
+    def __init__(self, airport):
+        self.airport_name = airport
+        self.routesTo = []
+        self.reachable = False
+        self.value = None
+
+
+class SpecialGraphAirportConnections:
+    def __init__(self, airports, routes, startingAirport):
+        self.airport_connections = self.buildAirportConnections(airports, routes)
+        self.starting_airport = self.airport_connections[startingAirport]
+
+    def buildAirportConnections(self, airports, routes):
+        airport_connections = {}
+        for airport in airports:
+            if airport not in airport_connections:
+                airport_connections[airport] = AirportConnectionNode(airport)
+        for route in routes:
+            start_airport, end_airport = route[0], route[1]
+            if start_airport not in airport_connections:
+                airport_connections[start_airport] = AirportConnectionNode(start_airport)
+            if end_airport not in airport_connections:
+                airport_connections[end_airport] = AirportConnectionNode(end_airport)
+            airport_connections[start_airport].routesTo.append(airport_connections[end_airport])
+        return airport_connections
+
+    def markUnreachableAirports(self, start_node):
+        if start_node.reachable:
+            return
+        start_node.reachable = True
+        start_node.value = 0
+        for next_airport in start_node.routesTo:
+            self.markUnreachableAirports(next_airport)
+'''
