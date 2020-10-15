b6e6f23e546dc07ce3738e47b8bd53d6a5dbe0a0
Ruslan <ruslankotvytskyi@gmail.com>
2020-10-15 12:52:35 +0800

    Create Check Palindrome Formation.py

---
+++ String/Check Palindrome Formation.py
@@ -0,0 +1,27 @@
+'''
+The idea here is that we just check with 2 pointers if letters from the start of string1 can be equvalent to the end letters of string2. 
+When we reach point where letters are different, we check we the rest of string1 or string2 is palindrom.
+'''
+
+class Solution:
+    def checkPalindromeFormation(self, a: str, b: str) -> bool:
+        return self.checkPalFromTwoStrings(a, b) or self.checkPalFromTwoStrings(b, a)
+    
+    def checkPalFromTwoStrings(self, str1, str2):
+        p1 = 0
+        p2 = len(str2)-1
+        while p2-p1 > 0 and str1[p1] == str2[p2]:
+            p2 -= 1
+            p1 += 1
+        
+        return self.checkPalOneString(str1, p2, p1) or self.checkPalOneString(str2, p2, p1)
+    
+    def checkPalOneString(self, string, p2, p1):
+        while p2-p1 > 0:
+            if string[p1] != string[p2]:
+                return False
+            p2 -= 1
+            p1 += 1
+        return True
+            
+        
