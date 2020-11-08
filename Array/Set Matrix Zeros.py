7567a7544f6c479c9a600558bb9e43067b7274d6
Ruslan <ruslan.kotvytskyi@gmail.com>
2020-11-08 17:33:31 +0800

    Update Set Matrix Zeros.py

--- Array/Set Matrix Zeros.py
+++ Array/Set Matrix Zeros.py
@@ -3,42 +3,120 @@ Given a matrix, A of size M x N of 0s and 1s. If an element is 0, set its entire
 
 Note: This will be evaluated on the extra memory used. Try to minimize the space and time complexity.
 
-
-
 Input Format:
 
 The first and the only argument of input contains a 2-d integer matrix, A, of size M x N.
 Output Format:
 
 Return a 2-d matrix that satisfies the given conditions.
+
+    >>> matrix = [[1, 1, 0, 0], [0, 1, 0, 1], [1, 1, 1, 1]]
+    >>> print(setZeroes(matrix))
+
+
 Constraints:
 
-1 <= N, M <= 1000
-0 <= A[i][j] <= 1
-Examples:
+    1 <= N, M <= 1000
+    0 <= A[i][j] <= 1
+    Examples:
 
-Input 1:
-    [   [1, 0, 1],
-        [1, 1, 1], 
-        [1, 1, 1]   ]
+    Input 1:
+        [   [1, 0, 1],
+            [1, 1, 1], 
+            [1, 1, 1]   ]
 
-Output 1:
-    [   [0, 0, 0],
-        [1, 0, 1],
-        [1, 0, 1]   ]
+    Output 1:
+        [   [0, 0, 0],
+            [1, 0, 1],
+            [1, 0, 1]   ]
 
-Input 2:
-    [   [1, 0, 1],
-        [1, 1, 1],
-        [1, 0, 1]   ]
+    Input 2:
+        [   [1, 0, 1],
+            [1, 1, 1],
+            [1, 0, 1]   ]
 
-Output 2:
-    [   [0, 0, 0],
-        [1, 0, 1],
-        [0, 0, 0]   ]
+    Output 2:
+        [   [0, 0, 0],
+            [1, 0, 1],
+            [0, 0, 0]   ]
 '''
+# O(R*C) Time / O(1) Space
+# But 3 times R*C
+# Using None as marker for zeros changes
 
+def setZeroes_v2(matrix):
 
+    def _setRowZeros(matrix, row):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] != 0:
+                matrix[row][col] = None
+
+    def _setColZeros(matrix, col):
+        for row in range(len(matrix)):
+            if matrix[row][col] != 0:
+                matrix[row][col] = 0
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] == 0:
+                _setRowZeros(matrix, row)
+                break
+    print(matrix)
+
+    for col in range(len(matrix[0])):
+        for row in range(len(matrix)):
+            if matrix[row][col] == 0:
+                _setColZeros(matrix, col)
+                break
+    print(matrix)
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] is None:
+                matrix[row][col] = 0
+
+    return matrix
+
+# O(R*C) Time / O(1) Space
+# But 2 times R*C 
+# Mark row and col on its zeros indies
+
+def setZeroes(matrix):
+    first_col_is_zero = False
+    first_row_is_zero = False
+
+    for col in range(len(matrix[0])):
+        if matrix[0][col] == 0:
+            first_col_is_zero = True
+            break
+
+    for row in range(len(matrix)):
+        if matrix[row][0] == 0:
+            first_row_is_zero = True
+            break
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] == 0:
+                matrix[row][0] = 0
+                matrix[0][col] = 0
+
+    for row in range(1, len(matrix)):
+        for col in range(1, len(matrix[0])):
+            if matrix[0][col] == 0 or matrix[row][0] == 0:
+                matrix[row][col] = 0
+
+    if first_col_is_zero:
+        for col in range(len(matrix[0])):
+            matrix[0][col] = 0
+
+    if first_row_is_zero:
+        for row in range(len(matrix)):
+            matrix[row][0] = 0
+
+    return matrix
+
+''' Old version - my first code
 class Solution:
     # @param A : list of list of integers
     # @return the same list modified
@@ -67,6 +145,6 @@ class Solution:
                         
         return A
         
-        
+'''
                     
                 
