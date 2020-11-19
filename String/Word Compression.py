72074646c402895e8daabafc38e6c376cd4807ec
Ruslan <ruslan.kotvytskyi@gmail.com>
2020-11-19 12:20:51 +0800

    Update Word Compression.py

--- String/Word Compression.py
+++ String/Word Compression.py
@@ -1,7 +1,28 @@
 '''
+compression()
 You need to compress word = 'aaaabbbccdkkrr' into
 'a3b3c2d1k2r2'.
+
+runLengthEncoding() # bound up to 9 symbols
+string = "AAAAAAAAAAAAABBCCCCDD"
+expected = "9A4A2B4C2D"
 '''
+
+# O(N) Time / O(N) Space
+
+def runLengthEncoding(string):
+    compressed_array = [1, string[0]]
+	for idx in range(1, len(string)):
+		if compressed_array[-1] != string[idx] or compressed_array[-2] == 9:
+			compressed_array.append(0)
+			compressed_array.append(string[idx])
+		compressed_array[-2] += 1
+		
+	for idx in range(0, len(compressed_array), 2):
+		compressed_array[idx] = str(compressed_array[idx])
+	
+	return ''.join(compressed_array)
+
 
 # O(N) Time / O(N) Space
 
