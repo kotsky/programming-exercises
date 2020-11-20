478eb8d1fe927c6e5a889bd9e2ccf29f87b93078
Ruslan <ruslan.kotvytskyi@gmail.com>
2020-11-20 18:20:04 +0800

    Update Underscorify Substring.py

--- String/Underscorify Substring.py
+++ String/Underscorify Substring.py
@@ -7,12 +7,33 @@ def underscorifySubstring(string, substring):
     special_symbol = '_'
     intervals = []
     lenSub = len(substring)
+
+# Version 1: O(m*n) Time due to compare substring
+# at each index of string
     # Step 1: find where we have substring
-    for i in range(len(string)):
-        if string[i:lenSub + i] == substring:
-            intervals.append([i, (lenSub + i)])
-    if intervals == []:
+#     for i in range(len(string)):
+#         if string[i:lenSub + i] == substring:
+#             intervals.append([i, (lenSub + i)])
+
+# Version 2: O(m + n) Time due to using str.find(),
+# which has O(m + n) Time, but every time we move
+# start point from the place of last appearence.
+# In average, we will go through symbols in string
+# 1-2 times - really depends on the input string 
+# and substring.
+    # Step 1: find where we have substring
+	start_idx = 0
+	while start_idx < len(string):
+		start_idx = string.find(substring, start_idx) 
+		if start_idx != -1:
+			intervals.append([start_idx, (lenSub + start_idx)])
+			start_idx += 1
+		else:
+			break
+		
+    if not intervals:
         return string
+
     # Step 2: merge intervals
     merged_interval = [intervals[0]]
     for i in range(1, len(intervals)):
